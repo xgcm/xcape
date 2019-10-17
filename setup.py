894b5e08a9ed6259635d7b140ab9647769fd64a8
Chiara Lepore <chiaralepore@gmail.com>
2019-10-16 22:39:46 -0400

    new setup.py2

--- setup.py
+++ setup.py
@@ -26,28 +26,28 @@ CLASSIFIERS = [
 INSTALL_REQUIRES = ['xarray>=0.12.0', 'dask', 'numpy>=1.16']
 PYTHON_REQUIRES = '>=3.6'
 
-ext_cape_ml = Extension(name = 'xcape.CAPE_CODE_model_lev',
+ext_cape_ml = Extension(name = 'CAPE_CODE_model_lev',
                         sources = ['xcape/CAPE_CODE_model_lev.pyf',
                                    'xcape/CAPE_CODE_model_lev.f90'])
-ext_cape_pl = Extension(name = 'xcape.CAPE_CODE_pressure_lev',
+ext_cape_pl = Extension(name = 'CAPE_CODE_pressure_lev',
                         sources = ['xcape/CAPE_CODE_pressure_lev.pyf',
                                    'xcape/CAPE_CODE_pressure_lev.f90'])
-ext_bunkers_ml = Extension(name = 'xcape.Bunkers_model_lev',
+ext_bunkers_ml = Extension(name = 'Bunkers_model_lev',
                         sources = ['xcape/Bunkers_model_lev.pyf',
                                    'xcape/Bunkers_model_lev.f90'])
-ext_bunkers_pl = Extension(name = 'xcape.Bunkers_pressure_lev',
+ext_bunkers_pl = Extension(name = 'Bunkers_pressure_lev',
                         sources = ['xcape/Bunkers_pressure_lev.pyf',
                                    'xcape/Bunkers_pressure_lev.f90'])
-ext_srh_ml = Extension(name = 'xcape.SREH_model_lev',
+ext_srh_ml = Extension(name = 'SREH_model_lev',
                         sources = ['xcape/SREH_model_lev.pyf',
                                    'xcape/SREH_model_lev.f90'])
-ext_srh_pl = Extension(name = 'xcape.SREH_pressure_lev',
+ext_srh_pl = Extension(name = 'SREH_pressure_lev',
                         sources = ['xcape/SREH_pressure_lev.pyf',
                                    'xcape/SREH_pressure_lev.f90'])
-ext_stdh_ml = Extension(name = 'xcape.stdheight_2D_model_lev',
+ext_stdh_ml = Extension(name = 'stdheight_2D_model_lev',
                         sources = ['xcape/stdheight_2D_model_lev.pyf',
                                    'xcape/stdheight_2D_model_lev.f90'])
-ext_stdh_pl = Extension(name = 'xcape.stdheight_2D_pressure_lev',
+ext_stdh_pl = Extension(name = 'stdheight_2D_pressure_lev',
                         sources = ['xcape/stdheight_2D_pressure_lev.pyf',
                                    'xcape/stdheight_2D_pressure_lev.f90'])
 # ext_cape_pl = Extension(name =  'xcape.Fortrancode',
@@ -90,7 +90,7 @@ setup(name=DISTNAME,
       # doesn't work for two extensions, only one
       # https://stackoverflow.com/questions/17744604/cython-producing-duplicate-symbols-pyinit-and-pyx-module-is-main
       #ext_modules = [ext_cape_ml, ext_cape_ml],
-      ext_package='pkg',
+      ext_package='xcape',
       ext_modules = [ext_cape_ml, ext_cape_pl, 
                      ext_bunkers_ml, ext_bunkers_pl, 
                      ext_srh_ml, ext_srh_pl, 
