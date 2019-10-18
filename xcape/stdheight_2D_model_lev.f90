7fa12ac674a1b8e8a4ada9f197990ad4021ba9d7
Chiara Lepore <chiaralepore@gmail.com>
2019-10-18 15:29:49 -0400

    clean bug in stdheight and update tests

--- xcape/stdheight_2D_model_lev.f90
+++ xcape/stdheight_2D_model_lev.f90
@@ -42,13 +42,15 @@
       DOUBLE PRECISION, INTENT(IN) :: Ps,Ts,Tds
       DOUBLE PRECISION, DIMENSION(NK) :: Tv
       DOUBLE PRECISION :: Tvs
-      DOUBLE PRECISION :: Tin
+      DOUBLE PRECISION :: Tin,Tdin,E,w
       DOUBLE PRECISION, INTENT(IN) :: Hin
       DOUBLE PRECISION, DIMENSION(NK), INTENT(OUT) :: H
       DOUBLE PRECISION, INTENT(OUT) :: Hs
 
       DOUBLE PRECISION, PARAMETER :: R=287.04
       DOUBLE PRECISION, PARAMETER :: g=-9.80665
+      INTEGER, PARAMETER :: n = 10
+      DOUBLE PRECISION, PARAMETER :: eps = 0.6219800858985514
       !PARAMETER (R=287.04,g=-9.80665)
       !Calculate Virtual Temperatures at the Two Levels and the mean of these two values.
       !Calculate Height using the Hyposometric Equation  RdTvmean/g (ln(p1/p2)) = z2-z1
@@ -58,15 +60,35 @@
 
       ! if surface level, use surface temperature
       Tin=Ts+273.15
-      Tvs=Tin/(1-0.379*(6.11*10*((7.5*Tds)/ &
-      (237.7+Tds))/Ps))
+      !W calculation starting from dewpoint
+      Tdin = Tds+273.15
+      E = 6.112*exp((17.67*(Tdin-273.15))/(Tds-29.65))
+      w = eps*(E/(Ps-E))
+      !Alt W calculation starting from specific humidity
+      ! w = (qs/1000)/(1-(qs/1000))
+      !Now Calculate Tv correctly:
+      Tvs = Tin*((w+eps)/(eps*(1+w)))
+      !Old Incorect Tv
+      !Tvs=Tin/(1-0.379*(6.11*10*((7.5*Tds)/ &
+      !(237.7+Tds))/Ps))
       ! loop along vertical levels 3D = NK
       DO k = 1, NK
         Tin=T(k)+273.15
+        !Calculate w for each layer
+        Tdin = Td(k)+273.15
+        ! E = 6.112*exp((17.67*(Tdin-273.15))/(Tds-29.65))
+        E = 6.112*exp((53.49-(6808/Tdin)-5.09*log(Tdin)))
+
+        w=eps*(E/(P(k)-E))
+        !Alt Calc w each layer
+        ! w= (qs/1000)/(1-(qs/1000))
         !Calculate Virtual Temperatures for a surface + 3d element
-        Tv(k)=Tin/(1-0.379*(6.11*10*((7.5*Td(k))/ &
-        (237.7+Td(k)))/P(k)))
+        Tv(k) = Tin*((w+eps)/(eps*(1+w)))
+        !Incorrect Old Method
+        !Tv(k)=Tin/(1-0.379*(6.11*10*((7.5*Td(k))/ &
+        !(237.7+Td(k)))/P(k)))
       ENDDO
+        !Now Integrate along the Hypsometric Equation
 
       DO k_ALL = 1, NK_ALL
         k = k_ALL-1
@@ -76,14 +98,23 @@
         ELSEIF (k_ALL .EQ. 2) THEN
           ! for k = 2 it uses Hs and Tvs for the layer below
           ! variables from 3D use k_ALL-1 as index
+          ! m = (Tv(k)-Tvs)/(log(P(k))-log(Ps))
+          ! b = Tvs-m*log(Ps)
+          ! call Trapz(m*x+b,log(Ps),log(P(k)),Tvp,n)
+          ! H(k)=Hs-((R/g)*Tvp)
+          ! original implementation assuming mean value theorem
           H(k)=Hs+((R*((Tv(k)+Tvs)/2)/g))*(log(P(k)/Ps))
         ELSE
           ! for k >= 3 it uses only 3D variables
           ! variables from 3D use k-1 as index
+          ! m = (Tv(k)-Tv(k-1))/(log(P(k))-log(P(k-1)))
+          ! b = Tv(k)-m*log(P(k))
+          ! call Trapz(Tv,log(P(k-1)),log(P(k)),Tvp,n)
+          ! H(k) = H(k-1)+((R/g)*Tvp)
           H(k)=H(k-1)+((R*((Tv(k)+Tv(k-1))/2)/g))*(log(P(k)/P(k-1)))
         ENDIF
       ENDDO
 
-      return
-      end subroutine stdheight_ml
-!END OF FILE STDHEIGHT.F
+      RETURN
+      END SUBROUTINE stdheight_ml
+      !END OF FILE STDHEIGHT.F
