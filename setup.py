1d346c8b2bf4898afbb35e075e38c1946b73db32
Chiara Lepore <chiaralepore@gmail.com>
2019-10-21 11:45:59 -0400

    added options to setup.py

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ import warnings
 import versioneer
 #from setuptools import setup, find_packages
 from numpy.distutils.core import setup, Extension
+from numpy.distutils.fcompiler import get_default_fcompiler, CompilerNotFound
 
 DISTNAME = 'xcape'
 LICENSE = 'MIT'
@@ -26,30 +27,65 @@ CLASSIFIERS = [
 INSTALL_REQUIRES = ['xarray>=0.12.0', 'dask', 'numpy>=1.16']
 PYTHON_REQUIRES = '>=3.6'
 
+try:
+        # figure out which compiler we're going to use
+        compiler = get_default_fcompiler()
+        # set some fortran compiler-dependent flags
+        f90flags = []
+        if compiler == 'gnu95':
+            f90flags.append('-fno-range-check')
+            f90flags.append('-ffree-form')
+        elif compiler == 'intel' or compiler == 'intelem':
+            f90flags.append('-132')
+        #  Set aggressive optimization level
+        f90flags.append('-O3')
+        #  Suppress all compiler warnings (avoid huge CI log files)
+        f90flags.append('-w')
+except :
+        print('No Fortran compiler found, not building the RRTMG_LW radiation module!')
+        build = False
+print(f90flags)
+
 ext_cape_ml = Extension(name = 'CAPE_CODE_model_lev',
                         sources = ['xcape/CAPE_CODE_model_lev.pyf',
-                                   'xcape/CAPE_CODE_model_lev.f90'])
+                                   'xcape/CAPE_CODE_model_lev.f90'],
+                             extra_f90_compile_args=f90flags,
+                             f2py_options=['--quiet'])
 ext_cape_pl = Extension(name = 'CAPE_CODE_pressure_lev',
                         sources = ['xcape/CAPE_CODE_pressure_lev.pyf',
-                                   'xcape/CAPE_CODE_pressure_lev.f90'])
+                                   'xcape/CAPE_CODE_pressure_lev.f90'],
+                             extra_f90_compile_args=f90flags,
+                             f2py_options=['--quiet'])
 ext_bunkers_ml = Extension(name = 'Bunkers_model_lev',
                         sources = ['xcape/Bunkers_model_lev.pyf',
-                                   'xcape/Bunkers_model_lev.f90'])
+                                   'xcape/Bunkers_model_lev.f90'],
+                             extra_f90_compile_args=f90flags,
+                             f2py_options=['--quiet'])
 ext_bunkers_pl = Extension(name = 'Bunkers_pressure_lev',
                         sources = ['xcape/Bunkers_pressure_lev.pyf',
-                                   'xcape/Bunkers_pressure_lev.f90'])
+                                   'xcape/Bunkers_pressure_lev.f90'],
+                             extra_f90_compile_args=f90flags,
+                             f2py_options=['--quiet'])
 ext_srh_ml = Extension(name = 'SREH_model_lev',
                         sources = ['xcape/SREH_model_lev.pyf',
-                                   'xcape/SREH_model_lev.f90'])
+                                   'xcape/SREH_model_lev.f90'],
+                             extra_f90_compile_args=f90flags,
+                             f2py_options=['--quiet'])
 ext_srh_pl = Extension(name = 'SREH_pressure_lev',
                         sources = ['xcape/SREH_pressure_lev.pyf',
-                                   'xcape/SREH_pressure_lev.f90'])
+                                   'xcape/SREH_pressure_lev.f90'],
+                             extra_f90_compile_args=f90flags,
+                             f2py_options=['--quiet'])
 ext_stdh_ml = Extension(name = 'stdheight_2D_model_lev',
                         sources = ['xcape/stdheight_2D_model_lev.pyf',
-                                   'xcape/stdheight_2D_model_lev.f90'])
+                                   'xcape/stdheight_2D_model_lev.f90'],
+                             extra_f90_compile_args=f90flags,
+                             f2py_options=['--quiet'])
 ext_stdh_pl = Extension(name = 'stdheight_2D_pressure_lev',
                         sources = ['xcape/stdheight_2D_pressure_lev.pyf',
-                                   'xcape/stdheight_2D_pressure_lev.f90'])
+                                   'xcape/stdheight_2D_pressure_lev.f90'],
+                             extra_f90_compile_args=f90flags,
+                             f2py_options=['--quiet'])
 
 DESCRIPTION = "Fast convective parameters for numpy, dask, and xarray"
 def readme():
@@ -74,4 +110,5 @@ setup(name=DISTNAME,
                      ext_bunkers_ml, ext_bunkers_pl, 
                      ext_srh_ml, ext_srh_pl, 
                      ext_stdh_ml, ext_stdh_pl]
+      
       )
