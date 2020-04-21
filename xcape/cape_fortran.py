316b0acc84a1c8fcfb81989e9853ec2bd56e280a
Ryan Abernathey <ryan.abernathey@gmail.com>
2020-04-21 10:37:45 -0400

    fix import

--- xcape/cape_fortran.py
+++ xcape/cape_fortran.py
@@ -1,27 +1,25 @@
+from . import CAPE_CODE_model_lev, CAPE_CODE_pressure_lev
 
 def cape(p_2d, t_2d, td_2d, p_s, t_s, td_s, flag_1d, pres_lev_pos, source, ml_depth, adiabat, pinc, type_grid):
-    
-                
+
+
     # nlev has to be the first dimension
     # nlev here is the number of levels in 3d variables (without surface level)
     nlev, ngrid = t_2d.shape
     # type_grid  type of vertical grid: 1 for model levels, 2 for pressure levels:
     if type_grid == 1:
-        from xcape import CAPE_CODE_model_lev
-    
         CAPE, CIN, MUlev, zMUlev = CAPE_CODE_model_lev.loopcape_ml(p_2d, t_2d, td_2d,
                                                       p_s, t_s, td_s,
                                                       pinc, source, ml_depth, adiabat,
                                                       nlev, ngrid)
     elif type_grid == 2:
-        from xcape import CAPE_CODE_pressure_lev    
-        if flag_1d ==0:
+        if flag_1d == 0:
             CAPE, CIN, MUlev, zMUlev = CAPE_CODE_pressure_lev.loopcape_pl(p_2d, t_2d, td_2d,
                                                               p_s, t_s, td_s,
                                                               pinc, source, ml_depth, adiabat,
                                                               pres_lev_pos,
                                                               nlev, ngrid)
-        elif flag_1d ==1:
+        elif flag_1d == 1:
             CAPE, CIN, MUlev, zMUlev = CAPE_CODE_pressure_lev.loopcape_pl1d(t_2d, td_2d, p_2d,
                                                               p_s, t_s, td_s,
                                                               pinc, source, ml_depth, adiabat,
