7785c6b2f2c37488ce92006d835f6e7e6023bc19
Ryan Abernathey <ryan.abernathey@gmail.com>
2020-10-29 17:29:41 -0400

    move fortran to subdirectory

--- src/xcape/stdheight.py
+++ src/xcape/stdheight.py
@@ -1,6 +1,9 @@
+import numpy as np
+from .fortran import stdheight_2D_model_lev, stdheight_2D_pressure_lev
+
 
 def stdheight(p_2d, t_2d, td_2d, p_s, t_s, td_s, flag_1d, pres_lev_pos, aglh0, type_grid):
-    import numpy as np
+
     # nlev has to be the first dimension
     # nlev here is the number of levels in 3d variables (without surface level)
     nlev, ngrid = t_2d.shape
@@ -12,16 +15,14 @@ def stdheight(p_2d, t_2d, td_2d, p_s, t_s, td_s, flag_1d, pres_lev_pos, aglh0, t
     else:
         # add check on shape
         aglh_in = aglh0
-        
+
     # type_grid  type of vertical grid: 1 for model levels, 2 for pressure levels:
     if type_grid == 1:
-        from xcape import stdheight_2D_model_lev
         H2D, H_s  = stdheight_2D_model_lev.loop_stdheight_ml(p_2d, t_2d, td_2d,
                                                       p_s, t_s, td_s,
                                                       aglh_in,
                                                       nlev, ngrid)
     elif type_grid == 2:
-        from xcape import stdheight_2D_pressure_lev
         # this option should not be triggered because of what is in core.py
 #         if flag_1d ==0:
 #             H2D, H_s  = stdheight_2D_pressure_lev.loop_stdheight_pl(p_2d, t_2d, td_2d,
@@ -29,9 +30,9 @@ def stdheight(p_2d, t_2d, td_2d, p_s, t_s, td_s, flag_1d, pres_lev_pos, aglh0, t
 #                                                       aglh_in,pres_lev_pos,
 #                                                       nlev, ngrid)
         if flag_1d ==1:
-            H2D, H_s  = stdheight_2D_pressure_lev.loop_stdheight_pl1d(t_2d, td_2d, p_2d, 
+            H2D, H_s  = stdheight_2D_pressure_lev.loop_stdheight_pl1d(t_2d, td_2d, p_2d,
                                                       p_s, t_s, td_s,
                                                       aglh_in,pres_lev_pos,
-                                                      nlev, ngrid)        
+                                                      nlev, ngrid)
 
     return H2D, H_s
